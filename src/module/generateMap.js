e3c5d8f192a4472658501ba4c186cf4618dfce72
ayoub <ayoubejraidi@gmail.com>
2024-10-20 19:57:01 +0100

    the cod whiith react and dist

---
+++ src/module/generateMap.js
@@ -0,0 +1,13 @@
+export function dogsObCreator(map, dogNum){
+  for(let i=0; i < dogNum; i++){
+    const dog = document.createElement('img')
+    dog.src ="../image/eskimo_dog-removebg-preview.png"
+    dog.classList.add('obstacle')
+    dog.style.left= Math.floor(Math.random()*100) + 'vw'
+    dog.style.top= Math.floor(Math.random()*100) +'vh'
+    map.appendChild(dog)
+    console.log( 
+      `Adding obstacle at ${dog.style.left}, ${dog.style.top}` 
+    );
+  }
+}
